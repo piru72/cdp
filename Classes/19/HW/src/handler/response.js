3b6354971a9ae46b2da980857510768a940af1ec
piru72 <parvezdirom2000@gmail.com>
2024-05-17 23:08:46 +0600

    Add : more response codes

--- Classes/19/HW/src/handler/response.js
+++ Classes/19/HW/src/handler/response.js
@@ -9,6 +9,17 @@ const responseHandler = () => {
         ctx.status = 201;
         ctx.body = { responseSummary, response };
       };
+      ctx.response.accepted = (responseSummary, response = {}) => {
+        ctx.status = 202;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.noContent = () => {
+        ctx.status = 204;
+        ctx.body = null;
+      };
+
+     
   
       ctx.response.badRequest = (responseSummary, response = {}) => {
         ctx.status = 400;
@@ -29,9 +40,46 @@ const responseHandler = () => {
         ctx.status = 404;
         ctx.body = { responseSummary, response };
       };
+
+      ctx.response.conflict = (responseSummary, response = {}) => {
+        ctx.status = 409;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.unprocessableEntity = (responseSummary, response = {}) => {
+        ctx.status = 422;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.tooManyRequests = (responseSummary, response = {}) => {
+        ctx.status = 429;
+        ctx.body = { responseSummary, response };
+      };
+
+
   
       ctx.response.internalError = (responseSummary, response = {}) => {
         ctx.status = 500;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.notImplemented = (responseSummary, response = {}) => {
+        ctx.status = 501;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.serviceUnavailable = (responseSummary, response = {}) => {
+        ctx.status = 503;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.gatewayTimeout = (responseSummary, response = {}) => {
+        ctx.status = 504;
+        ctx.body = { responseSummary, response };
+      };
+
+      ctx.response.custom = (status, responseSummary, response = {}) => {
+        ctx.status = status;
         ctx.body = { responseSummary, response };
       };
   
